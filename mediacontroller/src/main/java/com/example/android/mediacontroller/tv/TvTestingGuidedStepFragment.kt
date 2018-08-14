196a4c9d86128236b22cc13076e21d2fe292e98d
Nevin Mital <nevmital@google.com>
2018-08-14 15:17:48 -0700

    Added remaining tests to TV UI
    Bug: 110428802
    Change-Id: Ie9e3e82d79ae91e9424bca312a58bfa27ab15159

--- mediacontroller/src/main/java/com/example/android/mediacontroller/tv/TvTestingGuidedStepFragment.kt
+++ mediacontroller/src/main/java/com/example/android/mediacontroller/tv/TvTestingGuidedStepFragment.kt
@@ -19,8 +19,10 @@ import android.os.Bundle
 import android.support.v17.leanback.app.GuidedStepSupportFragment
 import android.support.v17.leanback.widget.GuidanceStylist
 import android.support.v17.leanback.widget.GuidedAction
+import android.support.v4.content.ContextCompat
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaControllerCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -29,7 +31,18 @@ import com.example.android.mediacontroller.MediaAppDetails
 import com.example.android.mediacontroller.R
 import com.example.android.mediacontroller.Test
 import com.example.android.mediacontroller.formatTvDetailsString
+import com.example.android.mediacontroller.runPauseTest
+import com.example.android.mediacontroller.runPlayFromMediaIdTest
+import com.example.android.mediacontroller.runPlayFromSearchTest
+import com.example.android.mediacontroller.runPlayFromUriTest
 import com.example.android.mediacontroller.runPlayTest
+import com.example.android.mediacontroller.runSeekToTest
+import com.example.android.mediacontroller.runSkipToItemTest
+import com.example.android.mediacontroller.runSkipToNextTest
+import com.example.android.mediacontroller.runSkipToPrevTest
+import com.example.android.mediacontroller.runStopTest
+import java.text.DateFormat
+import java.util.Date
 
 /**
  * Lists test options and displays selected MediaController details
@@ -38,6 +51,8 @@ class TvTestingGuidedStepFragment : GuidedStepSupportFragment() {
     private var mediaAppDetails: MediaAppDetails? = null
     private var mediaController: MediaControllerCompat? = null
     private var mediaBrowser: MediaBrowserCompat? = null
+
+    private var query: String = ""
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -70,39 +85,222 @@ class TvTestingGuidedStepFragment : GuidedStepSupportFragment() {
         )
     }
 
+    private fun buildAction(
+            id: Long,
+            title: String,
+            desc: String,
+            usesQuery: Boolean = false
+    ) : GuidedAction {
+        val action = GuidedAction.Builder(context)
+                .id(id)
+                .title(title)
+                .description(desc)
+                .multilineDescription(true)
+                .editTitle("")
+                .editable(usesQuery)
+                .hasNext(usesQuery)
+                .build()
+        return action
+    }
+
     override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
-        super.onCreateActions(actions, savedInstanceState)
-        actions.add(GuidedAction.Builder(context)
-                .title(getString(R.string.play_test_title))
-                .description(getString(R.string.play_test_desc))
-                .id(PLAY_TEST)
+        actions.add(buildAction(
+                PLAY_TEST,
+                getString(R.string.play_test_title),
+                getString(R.string.play_test_desc)
+        ))
+        actions.add(buildAction(
+                PLAY_FROM_SEARCH_TEST,
+                getString(R.string.play_search_test_title),
+                getQueryTestDesc(R.string.play_search_test_desc),
+                usesQuery = true
+        ))
+        actions.add(buildAction(
+                PLAY_FROM_MEDIA_ID_TEST,
+                getString(R.string.play_media_id_test_title),
+                getQueryTestDesc(R.string.play_media_id_test_desc),
+                usesQuery = true
+        ))
+        actions.add(buildAction(
+                PLAY_FROM_URI_TEST,
+                getString(R.string.play_uri_test_title),
+                getQueryTestDesc(R.string.play_uri_test_desc),
+                usesQuery = true
+        ))
+        actions.add(buildAction(
+                PAUSE_TEST,
+                getString(R.string.pause_test_title),
+                getString(R.string.pause_test_desc)
+        ))
+        actions.add(buildAction(
+                STOP_TEST,
+                getString(R.string.stop_test_title),
+                getString(R.string.stop_test_desc)
+        ))
+        actions.add(buildAction(
+                SKIP_TO_NEXT_TEST,
+                getString(R.string.skip_next_test_title),
+                getString(R.string.skip_next_test_desc)
+        ))
+        actions.add(buildAction(
+                SKIP_TO_PREV_TEST,
+                getString(R.string.skip_prev_test_title),
+                getString(R.string.skip_prev_test_desc)
+        ))
+        actions.add(buildAction(
+                SKIP_TO_ITEM_TEST,
+                getString(R.string.skip_item_test_title),
+                getQueryTestDesc(R.string.skip_item_test_desc),
+                usesQuery = true
+        ))
+        actions.add(buildAction(
+                SEEK_TO_TEST,
+                getString(R.string.seek_test_title),
+                getQueryTestDesc(R.string.seek_test_desc),
+                usesQuery = true
+        ))
+        actions.add(actions.size, GuidedAction.Builder(context)
+                .id(REFRESH_INFO)
+                .title(getString(R.string.logs_trigger_title))
                 .build()
         )
     }
 
+    // For tests that use the query field
+    override fun onGuidedActionEditedAndProceed(action: GuidedAction?): Long {
+        mediaController?.let {
+            query = action?.editTitle.toString()
+            when (action?.id) {
+                PLAY_FROM_SEARCH_TEST -> {
+                    action.title = getString(R.string.play_search_test_title)
+                    action.description = getQueryTestDesc(R.string.play_search_test_desc, query)
+                    runPlayFromSearchTest(query, it, ::logTestUpdate)
+                }
+                PLAY_FROM_MEDIA_ID_TEST -> {
+                    action.title = getString(R.string.play_media_id_test_title)
+                    action.description = getQueryTestDesc(R.string.play_media_id_test_desc, query)
+                    runPlayFromMediaIdTest(query, it, ::logTestUpdate)
+                }
+                PLAY_FROM_URI_TEST -> {
+                    action.title = getString(R.string.play_uri_test_title)
+                    action.description = getQueryTestDesc(R.string.play_uri_test_desc, query)
+                    runPlayFromUriTest(query, it, ::logTestUpdate)
+                }
+                SKIP_TO_ITEM_TEST -> {
+                    action.title = getString(R.string.skip_item_test_title)
+                    action.description = getQueryTestDesc(R.string.skip_item_test_desc, query)
+                    runSkipToItemTest(query, it, ::logTestUpdate)
+                }
+                SEEK_TO_TEST -> {
+                    action.title = getString(R.string.seek_test_title)
+                    action.description = getQueryTestDesc(R.string.seek_test_desc, query)
+                    runSeekToTest(query, it, ::logTestUpdate)
+                }
+                else -> {
+                    query = ""
+                    Toast.makeText(
+                            activity,
+                            getString(R.string.tv_invalid_test),
+                            Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        return GuidedAction.ACTION_ID_CURRENT
+    }
+
+    override fun onGuidedActionEditCanceled(action: GuidedAction?) {
+        query = ""
+        when (action?.id) {
+            PLAY_FROM_SEARCH_TEST -> {
+                action.title = getString(R.string.play_search_test_title)
+                action.description = getQueryTestDesc(R.string.play_search_test_desc)
+            }
+            PLAY_FROM_MEDIA_ID_TEST -> {
+                action.title = getString(R.string.play_media_id_test_title)
+                action.description = getQueryTestDesc(R.string.play_media_id_test_desc)
+            }
+            PLAY_FROM_URI_TEST -> {
+                action.title = getString(R.string.play_uri_test_title)
+                action.description = getQueryTestDesc(R.string.play_uri_test_desc)
+            }
+            SKIP_TO_ITEM_TEST -> {
+                action.title = getString(R.string.skip_item_test_title)
+                action.description = getQueryTestDesc(R.string.skip_item_test_desc)
+            }
+            SEEK_TO_TEST -> {
+                action.title = getString(R.string.seek_test_title)
+                action.description = getQueryTestDesc(R.string.seek_test_desc)
+            }
+            else -> Toast.makeText(
+                    activity,
+                    getString(R.string.tv_invalid_test),
+                    Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    // For tests that do not use the query field
     override fun onGuidedActionClicked(action: GuidedAction?) {
-        super.onGuidedActionClicked(action)
+        val queryTests = longArrayOf(
+                PLAY_FROM_SEARCH_TEST,
+                PLAY_FROM_MEDIA_ID_TEST,
+                PLAY_FROM_URI_TEST,
+                SKIP_TO_ITEM_TEST,
+                SEEK_TO_TEST
+        )
         mediaController?.let {
             when (action?.id) {
                 PLAY_TEST -> runPlayTest(it, ::logTestUpdate)
-                else -> Toast.makeText(
-                        activity,
-                        getString(R.string.tv_invalid_test),
-                        Toast.LENGTH_LONG
-                ).show()
+                PAUSE_TEST -> runPauseTest(it, ::logTestUpdate)
+                STOP_TEST -> runStopTest(it, ::logTestUpdate)
+                SKIP_TO_NEXT_TEST -> runSkipToNextTest(it, ::logTestUpdate)
+                SKIP_TO_PREV_TEST -> runSkipToPrevTest(it, ::logTestUpdate)
+                REFRESH_INFO -> guidanceStylist.descriptionView.text = it.formatTvDetailsString()
+                else -> {
+                    if (action?.id == null || !queryTests.contains(action.id)) {
+                        Toast.makeText(
+                                activity,
+                                getString(R.string.tv_invalid_test),
+                                Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
             }
         }
 
     }
 
     private fun logTestUpdate(logTag: String, message: String) {
+        val date = DateFormat
+                .getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG)
+                .format(Date())
+        Log.i("TvTestingFragment", "[$date] <$logTag>:\n$message")
         activity?.runOnUiThread {
             guidanceStylist.descriptionView.text = mediaController?.formatTvDetailsString()
             guidanceStylist.breadcrumbView.text = message
         }
     }
 
+    private fun getQueryTestDesc(id: Int, query: String? = null): String {
+        return if (query == null) {
+            getString(R.string.tv_text_input, getString(id))
+        } else {
+            getString(R.string.tv_text_input_with_query, getString(id), query)
+        }
+    }
+
     companion object {
+        const val REFRESH_INFO = -1L
         const val PLAY_TEST = 0L
+        const val PLAY_FROM_SEARCH_TEST = 1L
+        const val PLAY_FROM_MEDIA_ID_TEST = 2L
+        const val PLAY_FROM_URI_TEST = 3L
+        const val PAUSE_TEST = 4L
+        const val STOP_TEST = 5L
+        const val SKIP_TO_NEXT_TEST = 6L
+        const val SKIP_TO_PREV_TEST = 7L
+        const val SKIP_TO_ITEM_TEST = 8L
+        const val SEEK_TO_TEST = 9L
     }
 }
