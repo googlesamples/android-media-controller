4863d8b573c42bf5a3ca8d4e2728edd501f1bf9d
Leo Neat <lneat@google.com>
2020-05-28 11:36:57 -0700

    Removing resource import

--- mediacontroller/src/main/java/com/example/android/mediacontroller/MediaAppTestingActivity.kt
+++ mediacontroller/src/main/java/com/example/android/mediacontroller/MediaAppTestingActivity.kt
@@ -19,7 +19,6 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
